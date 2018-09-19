44fbcad9f2656cd24331fc3de3a1365124aca884
DirtyOleg <11252872+DirtyOleg@users.noreply.github.com>
2018-09-18 21:20:59 -0400

    .

---
+++ Test Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Test_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Record r = new Record() { id = 0 };
+            
+            Console.WriteLine($"({Marshal.SizeOf(r)})");
+            Console.Read();
+        }
+    }
+
+    struct Record
+    {
+        public int id;
+        const string nothing = "Place Holder";
+    }
+}
