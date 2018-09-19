c06ebe121ebc8561acf2f85c398a637b234fd056
DirtyOleg <11252872+DirtyOleg@users.noreply.github.com>
2018-09-18 21:22:40 -0400

    .

---
+++ Random Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[2];
+            persons[0] = new Person { Age = 18, Name = "David" };
+            persons[1] = new Person { Age = 20, Name = "Elam" };
+
+            Person[] other = new Person[2];
+            other = persons;
+            Change(other);
+            Console.WriteLine(other[0].Age);
+            Console.Read();
+        }
+
+        static void Change(Person[] p)
+        {
+            p[0].Age = 99; 
+        }
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
